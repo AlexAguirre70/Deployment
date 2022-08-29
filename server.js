2d64bf8437b8333d422cc08f72f45c7db9ca1167
Alex Aguirre <teamaguirre@gmail.com>
2022-08-29 01:39:22 -0700

    Heroku deployment

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const { response } = require('express')
 const express =require('express')
 require('dotenv').config()
 const app = express()
-
+// Routes
 app.get('/', (req,res)=> {
-    res.send('Hello World of HeROKU')
+    res.send('Hello World of Heroku')
 })
 
 app.listen(process.env.PORT)
